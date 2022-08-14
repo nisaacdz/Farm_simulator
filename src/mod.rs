7a97843cde77745dce096cd62690d362ba364eb4
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2022-08-14 06:47:27 +0000

    Second commit

---
+++ src/mod.rs
@@ -0,0 +1,21 @@
+enum CropType {
+    CEREALS,
+    VEGETABLES,
+    FRUITS,
+    DUGS,
+    WOOD,
+}
+
+enum CEREALS {
+    MAIZE,
+    WHEAT,
+    RICE,
+    
+}
+
+enum Product {
+    
+}
+mod Farm {
+    
+}
